8b4e6d86173a0e9916e62653b040371b4eeb7051
tsujin <contact@tsujin.com>
2025-09-05 04:22:05 -0400

    added bashrc

--- setup.sh
+++ setup.sh
@@ -11,3 +11,15 @@ if [ -L "$VIMRC_TARGET" ]; then
 else
     echo "Failed to create link for '$VIMRC_TARGET'."
 fi
+
+# Bashrc
+BASHRC_TARGET="$HOME/.bashrc"
+BASHRC_LINK="$HOME/dotfiles/bash/.bashrc"
+
+ln -s "$BASHRC_LINK" "$BASHRC_TARGET"
+
+if [ -L "$BASHRC_TARGET" ]; then
+    echo "Link '$BASHRC_TARGET' created successfully."
+else
+    echo "Failed to create link for '$BASHRC_TARGET'."
+fi
